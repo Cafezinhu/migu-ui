9f13a2c7c3ee45f93c952d008fde5cfac558f91f
Aerton <aertoncastro@gmail.com>
2022-07-21 17:47:20 -0300

    example with camera

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -3,9 +3,24 @@ import './style.css';
 import { Button, MiguUI } from '../../src/index';
 import coffee from '../coffee.png';
 import google from '../google.png';
+import { Viewport } from 'pixi-viewport';
 async function load(){
   const app = new Application({resizeTo: window});
   document.body.appendChild(app.view);
+
+  const viewport = new Viewport({
+    screenHeight: window.innerHeight,
+    screenWidth: window.innerWidth,
+    worldWidth: window.innerWidth,
+    worldHeight: window.innerHeight,
+    //@ts-ignore
+    interaction: app.renderer.plugins.interaction
+  });
+
+  viewport.drag().wheel();
+
+  app.stage
+  .addChild(viewport);
 
   app.stage.filters = [new filters.FXAAFilter()];
 
@@ -28,6 +43,8 @@ async function load(){
   const googleTexture = await Texture.fromURL(google);
   element.addContent([new Sprite(coffeeTexture), new Sprite(googleTexture)]);
   ui.addElement(element);
+
+  viewport.addChild(new Sprite(await Texture.fromURL(coffee)));
 
   window.addEventListener('resize', resize);
 
